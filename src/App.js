d885ae10d98b4c5dffcf195b77cb80f9b5148f2f
Manoj AP <manojap.nair@gmail.com>
2022-07-06 23:16:03 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import "./sparkle-element";
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <sparkle-element />
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
